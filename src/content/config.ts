91a9a3464b9d56c2b5d3b4390105a7157d31e55f
Paulo Preto <paulopreto.dev@gmail.com>
2023-09-26 23:16:35 +0100

    Moved posts to content folder and add collection schema

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const postCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string())
+  })
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'post': postCollection,
+};
