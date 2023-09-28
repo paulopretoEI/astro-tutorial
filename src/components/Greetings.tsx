9438da854f97baabc32e903c1eb23572809ae401
Paulo Preto <paulopreto.dev@gmail.com>
2023-09-28 22:39:58 +0100

    Update greetings component

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -5,15 +5,25 @@ type Props = {
 };
 
 export default function Greeting({ messages }: Props) {
-  const randomMessage = () =>
-    messages[Math.floor(Math.random() * messages.length)];
-
+  const randomMessage = () => {
+    let newGreeting;
+    do {
+      newGreeting = messages[Math.floor(Math.random() * messages.length)];
+    } while (newGreeting === greeting);
+    return newGreeting;
+  };
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+      <button
+        type="button"
+        onClick={() => setGreeting(randomMessage())}
+        data-cy="new-greeting"
+      >
+        New Greeting
+      </button>
     </div>
   );
 }
